8968d400e7c34ec7e8debb6564197ded2cab4adf
Osman Herman <osmanherman27@gmail.com>
2022-12-17 16:27:14 -0300

    Fin del trabajo

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+function capturar(){
+    function Persona(nombre,edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    var nombreCapturar = document.getElementById("nombre").value;
+    var edadCapturar = document.getElementById("edad").value;
+
+    nuevoSujeto = new Persona(nombreCapturar,edadCapturar);
+    console.log(nuevoSujeto);
+    agregar();
+}
+
+var baseDatos = [];
+function agregar(){
+    baseDatos.push(nuevoSujeto);
+    console.log(baseDatos);
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevoSujeto.nombre+'</td><td>'+nuevoSujeto.edad+'</td></tbody>';
+}
